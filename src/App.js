9268f0ecafa511035cb161df858c2e27243abd40
ImanRS1 <omid-omega@mail.com>
2021-02-15 11:18:09 +0100

    added globalstyle and styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import About from "./pages/About";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <About />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,67 @@
 import React from "react";
 import home1 from "../img/placeholder-img.jpg";
+import styled from "styled-components";
+import { faFileExcel } from "@fortawesome/free-solid-svg-icons";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <StyledAbout>
+      <StyledDescription>
         <div className="title">
-          <div className="hide">
+          <StyledHide className="StyledHide">
             <h2>lorem 1</h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide className="StyledHide">
             <h2>
               lor<span>rem</span> 3
             </h2>
-          </div>
-          <div className="hide">
+          </StyledHide>
+          <StyledHide className="StyledHide">
             <h2>lorem 3</h2>
-          </div>
+          </StyledHide>
         </div>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium
           praesentium deserunt amet?
         </p>
         <button>Contact Iman</button>
-      </div>
-      <div className="image">
+      </StyledDescription>
+      <StyledImage>
         <img src={home1} alt="Image of Iman Reza Soltani" />
-      </div>
-    </div>
+      </StyledImage>
+    </StyledAbout>
   );
 };
+
+const StyledAbout = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 150%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const StyledHide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
