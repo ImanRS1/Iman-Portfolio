1b4fb234e6ba8bfc4f589ff2a61aa3104d403604
ImanRS1 <omid-omega@mail.com>
2021-02-16 20:43:25 +0100

    added more framer animation to aboutsection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import home1 from "../img/placeholder-img.jpg";
 import {motion} from "framer-motion";
+import {titleAnim, fade, imgAnim} from "../animation";
 import {
   StyledAbout,
   StyledDescription,
@@ -14,25 +15,25 @@ const AboutSection = () => {
       <StyledDescription>
         <motion.div className="title">
           <StyledHide className="StyledHide">
-            <motion.h2>lorem 1</motion.h2>
+            <motion.h2 variants={titleAnim}>lorem 1</motion.h2>
           </StyledHide>
           <StyledHide className="StyledHide">
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               lor<span>rem</span> 3
             </motion.h2>
           </StyledHide>
           <StyledHide className="StyledHide">
-            <motion.h2>lorem 3</motion.h2>
+            <motion.h2 variants={titleAnim}>lorem 3</motion.h2>
           </StyledHide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium
           praesentium deserunt amet?
-        </p>
-        <button>Contact Iman</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Iman</motion.button>
       </StyledDescription>
       <StyledImage>
-        <img src={home1} alt="Image of Iman Reza Soltani" />
+        <motion.img src={home1} alt="Image of Iman Reza Soltani" variants={imgAnim} />
       </StyledImage>
     </StyledAbout>
   );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,17 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import AttributeSection from "../components/AttributeSection";
-import {motion} from "framer-motion";
-import {pageAnimation} from "../animation";
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const About = () => {
   return (
-    <motion.div variants={pageAnimation} initial="hidden" animate="show" exit="exit">
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
       <AboutSection />
       <AttributeSection />
     </motion.div>
