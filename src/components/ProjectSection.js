e0beb5000dd229ca43fa8d9206f5b199d75edb0d
ImanRS1 <omid-omega@mail.com>
2021-05-03 16:32:21 +0200

    link icon anim added to fp3

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -342,7 +342,13 @@ const ProjectSection = () => {
                   initial="hidden"
                   animate="show"
                 >
-                  <FontAwesomeIcon icon={faGithub} color="#fafafa" />
+                  <motion.div
+                    variants={linkIconAnim1}
+                    initial="hidden"
+                    animate="show"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </motion.div>
                 </motion.a>
                 <motion.a
                   href="https://github.com/ImanRS1/React-Music-Player"
@@ -352,7 +358,13 @@ const ProjectSection = () => {
                   initial="hidden"
                   animate="show"
                 >
-                  <FontAwesomeIcon icon={faCode} color="#fafafa" />
+                  <motion.div
+                    variants={linkIconAnim2}
+                    initial="hidden"
+                    animate="show"
+                  >
+                    <FontAwesomeIcon icon={faCode} />
+                  </motion.div>
                 </motion.a>
               </motion.div>
             </div>
