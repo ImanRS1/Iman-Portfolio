72f6f2d343d46eb95cf176ceece5e015d2c378d5
ImanRS1 <omid-omega@mail.com>
2021-10-29 18:40:55 +0200

    atrSec updated

--- src/components/AttributeSection.js
+++ src/components/AttributeSection.js
@@ -52,7 +52,7 @@ const AttributeSection = () => {
                 <FontAwesomeIcon icon={faPlane} color="#23d997" />
                 <h3>International</h3>
               </div>
-              <StyledAttriP>International work experience stretching over 22 countries</StyledAttriP>
+              <StyledAttriP>International work experience stretching over 25 countries</StyledAttriP>
             </StyledCard>
           </StyledHide>
 
